08dc10d1ec3aba15f133cca6147f1c9a5061c1ee
wannabewize <wannabewize@gmail.com>
2018-12-31 02:59:13 +0900

    hello2 feature

--- server.js
+++ server.js
@@ -5,9 +5,12 @@ const goodNight = require('./goodNight2');
 app.use(goodNight);
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 const hello3 = require('./hello3');
 app.use(hello3);
 
+=======
+>>>>>>> hello2 feature
 =======
 >>>>>>> hello2 feature
 const hello2 = require('./hello2');
