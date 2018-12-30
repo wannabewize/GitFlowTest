7bf8f50dd2b6f2832d0ac2bfd84ee1467d6c4248
wannabewize <wannabewize@gmail.com>
2018-12-31 02:56:35 +0900

    초기화

--- server.js
+++ server.js
@@ -1,12 +1,4 @@
 const express = require('express');
 const app = express();
 
-
-const hello = require('./hello');
-app.use(hello);
-
-app.get('/goodNight', (req, res) => {
-    res.send('good night~ sweet dream!');
-});
-
 app.listen(3000);
